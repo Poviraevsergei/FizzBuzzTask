5eac6bd356116e6fc44ee224a899df0589b015c4
Poviraevsergei <pacmanmozg1441>
2021-11-09 12:09:32 +0300

    Added solution with using rules

--- src/main/java/FizzBuzzTask.java
+++ src/main/java/FizzBuzzTask.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -10,5 +11,22 @@ public class FizzBuzzTask {
         return IntStream.rangeClosed(START_RANGE, END_RANGE)
                 .mapToObj(i -> i % 5 == 0 ? (i % 3 == 0 ? "FizzBuzz" : "Buzz") : (i % 3 == 0 ? "Fizz" : String.valueOf(i)))
                 .collect(Collectors.joining(" "));
+    }
+
+    public String solutionWithRule(Map<Integer, String> rules) {
+        return IntStream.rangeClosed(START_RANGE, END_RANGE)
+                .mapToObj(number -> changeValueUsingRule(number, rules))
+                .collect(Collectors.joining(" "));
+    }
+
+    public String changeValueUsingRule(int number, Map<Integer, String> rules) {
+        StringBuilder result = new StringBuilder(number);
+        for (Map.Entry<Integer, String> rule : rules.entrySet()) {
+            if ((number % rule.getKey()) == 0) {
+                result.append(rule.getValue());
+            }
+        }
+
+        return String.valueOf(result.toString().isBlank() ? result.append(number) : result);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        Map<Integer, String> rules = new HashMap<>();
+        rules.put(3, "Fizz");
+        rules.put(5, "Buzz");
+        rules.put(7, "Jazz");
+
         FizzBuzzTask fizzBuzzTask = new FizzBuzzTask();
 
         //Common solution
         System.out.println(fizzBuzzTask.solution());
+
+        //Solution with rules
+        System.out.println(fizzBuzzTask.solutionWithRule(rules));
     }
 }
